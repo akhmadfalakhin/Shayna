16f951254ea1ad5b2cf200303ba251d94e84093f
Akhmad Falakhin <akhmadfalakhin22@gmail.com>
2020-12-22 23:17:23 +0700

    Shayna

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <Hero />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <Footer />
+       
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import Hero from '@/components/Hero.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/Partner.vue';
+import Footer from '@/components/Footer.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    Hero,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    Footer
+  }
+}
+</script>
